4435fdc5b09bd74e779e5e245160b0161660dcde
Junsuk Oh <a811219@gmail.com>
2021-03-23 15:38:43 +0900

    6 ~ 10 까지 주사위 추가

--- lib/3_23.dart
+++ lib/3_23.dart
@@ -4,8 +4,28 @@ void main() {
   print('주사위 : ${dice()}');
 }
 
-// 주사위
-int dice() {
+// 1~10 주사위
+int dice10() {
+  Random random = Random();
+  return random.nextInt(10) + 1;
+}
+// 1~6 주사위
+int dice6() {
   Random random = Random();
   return random.nextInt(6) + 1;
+}
+// 1~7 주사위
+int dice7() {
+  Random random = Random();
+  return random.nextInt(7) + 1;
+}
+// 1~8 주사위
+int dice8() {
+  Random random = Random();
+  return random.nextInt(8) + 1;
+}
+// 1~9 주사위
+int dice9() {
+  Random random = Random();
+  return random.nextInt(9) + 1;
 }
