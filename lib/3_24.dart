481de6801b44028ce316750f9aa26c9339283ba4
Junsuk Oh <a811219@gmail.com>
2021-03-24 12:23:04 +0900

    리스트 활용 이런 저런 기능들

--- lib/3_24.dart
+++ lib/3_24.dart
@@ -12,6 +12,7 @@ void main() {
 
 int total(List<int> scores) {
   int result = 0;
+  // 반복문
   for (int i = 0; i < scores.length; i++) {
     result = result + scores[i];
   }
@@ -20,4 +21,30 @@ int total(List<int> scores) {
 
 double avg(List<int> scores) {
   return total(scores) / scores.length;
+}
+
+void listExam() {
+  List<int> scores = [100, 50, 70, 30];
+
+  // 맨 뒤에 30 추가
+  scores.add(40); // 100, 50, 70, 30, 40
+
+  // 40을 뺀다
+  scores.remove(40); // 100, 50, 70, 30
+
+  // 3번 인덱스의 값 삭제
+  scores.removeAt(3); // 100, 50, 70
+
+  // 맨 앞에것 삭제
+  scores.removeAt(0); // 50, 70
+
+  // 0번째에 35 추가
+  scores.insert(0, 35); // 35, 50, 70
+
+  // 모두 삭제
+  scores.clear(); // 아무것도 없음
+
+  // 뒤에다가 리스트를 붙인다
+  scores.addAll([10, 20, 30]);  // 10, 20, 30
+
 }
