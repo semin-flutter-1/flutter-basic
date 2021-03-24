85142159f5e197bab97a0c05be0a63a61be0b795
Junsuk Oh <a811219@gmail.com>
2021-03-24 12:07:18 +0900

    성적 계산 프로그램 5 - 리스트 도입

--- lib/3_24.dart
+++ lib/3_24.dart
@@ -3,17 +3,21 @@
 void main() {
   List<int> scores = [100, 50, 70, 30];
 
-  int sum = total(scores[0], scores[1], scores[2], scores[3]);
-  double average = avg(scores[0], scores[1], scores[2], scores[3]);
+  int sum = total(scores);
+  double average = avg(scores);
 
   print('합계 : $sum');
   print('평균 : ${average.toStringAsFixed(2)}');
 }
 
-int total(int korean, int math, int english, int science) {
-  return korean + math + english + science;
+int total(List<int> scores) {
+  int result = 0;
+  for (int i = 0; i < scores.length; i++) {
+    result = result + scores[i];
+  }
+  return result;
 }
 
-double avg(int korean, int math, int english, int science) {
-  return total(korean, math, english, science) / 4;
+double avg(List<int> scores) {
+  return total(scores) / scores.length;
 }
