1ebeaf42f0175bfc067d92c3944c926753796871
Junsuk Oh <a811219@gmail.com>
2021-03-24 11:14:59 +0900

    함수 문제 풀이

--- lib/3_23.dart
+++ lib/3_23.dart
@@ -1,19 +1,52 @@
+import 'dart:io';
 import 'dart:math';
 
 void main() {
-  print('주사위 : ${dice(start: 5, end: 6)}');
-  print('주사위 : ${dice(end: 5)}');
+  // 파라미터는 뭐지?? => String 3개 (제목, 주소, 본문)
+  email(title: '안녕하세요',
+      address: 'abc@abc.com',
+      text: '냉무');
+
+  email(address: 'abc@abc.com', text: '냉무');
+
+  // print(calcTriangleArea(10, 5));
+  //
+  // double result = calcTriangleArea(20, 30);
+  print('반지름을 입력하세요');
+  double radius = double.parse(stdin.readLineSync());
+  print('반지름이 $radius인 원의 넓이는 ${calcCircleArea(radius)}');
+
+
+}
+
+// PI * R ^ 2
+double calcCircleArea(double radius) {
+  return pi * radius * radius;
+}
+
+double calcTriangleArea(double bottom, double height) {
+  double result = bottom * height / 2;
+  return result;
+}
+
+double calcTriangleArea2(double bottom, double height) {
+  return bottom * height / 2;
+}
+
+double calcTriangleArea3(double bottom, double height)
+  => bottom * height / 2;
+
+void email({String title, String address, String text}) {
+  // title 인자로 들어온 값은 뭐야?
+  print('$address 에 아래의 메일을 송신한다. ');
+  print('제목 : $title');
+  print('본문 : $text');
 }
 
 // 정수 num 하나를 받아서 정수 1 ~ num 를 반환한다
 // && 그리고, || 또는
 // 매개변수를 {} 로 감싸면 선택적으로 매개변수를 선택할 수 있게 된다. (선택적 파라미터)
-int dice({int start, int end}) {
-  if (start != null && end == null) {
-    end = 6;
-  } else if (start == null && end != null) {
-    start = 0;
-  }
+int dice(int start, int end) {
   return Random().nextInt(end - start + 1) + start;
 }
 
--- lib/exam2.dart
+++ lib/exam2.dart
@@ -1,8 +1,41 @@
 import 'dart:io';
+import 'dart:math';
 
 // 메인 함수
 void main() {
-  q4();
+  // 파라미터는 뭐지?? => String 3개 (제목, 주소, 본문)
+  // email('안녕하세요', 'abc@abc.com', '냉무');
+  // print(calcTriangleArea(10, 5));
+  //
+  // double result = calcTriangleArea(20, 30);
+  // print('반지름을 입력하세요');
+  // double radius = double.parse(stdin.readLineSync());
+  // print('반지름이 $radius인 원의 넓이는 ${calcCircleArea(radius)}');
+
+  // 파라미터는 뭐지?? => String 3개 (제목, 주소, 본문)
+  // email(title: '안녕하세요',
+  //     address: 'abc@abc.com',
+  //     text: '냉무');
+
+  email(text: '냉무', address: '');
+}
+
+void email({String title, String address, String text}) {
+  // title 인자로 들어온 값은 뭐야?
+  print('$address 에 아래의 메일을 송신한다. ');
+
+  if (title == null) {
+    title = '제목 없음';
+  }
+
+  print('제목 : $title');
+  print('본문 : $text');
+}
+
+// PI * R ^ 2
+double calcCircleArea(double radius) {
+  double result = pi * radius * radius;
+  return result;
 }
 
 // 함수
