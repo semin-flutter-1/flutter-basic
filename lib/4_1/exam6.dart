4b2be78a5eb6973caea5141cc25b9420cda04fe3
Junsuk Oh <a811219@gmail.com>
2021-04-01 15:22:27 +0900

    문제풀이

--- lib/4_1/exam6.dart
+++ lib/4_1/exam6.dart
@@ -1,26 +1,7 @@
 import 'dart:io';
 
 void main() {
-  int percentage = int.parse(stdin.readLineSync());
-  Exam exam = Exam(percentage);
-  print(exam.solution());
-  print(exam.solution());
-}
+  List<String> name = stdin.readLineSync().split(' ');
 
-class Exam {
-  int a;
-
-  Exam(this.a);
-
-  String solution() {
-    String result;
-    if (0 <= a && a <= 30) {
-      result = 'sunny';
-    } else if (a >= 31 && a <= 70) {
-      result = 'cloudy';
-    } else {
-      result = 'rainy';
-    }
-    return result;
-  }
+  print('${name[0][0]}.${name[1][0]}');
 }
