c0423b3d01607db697e637316e56dd19bd85b652
Junsuk Oh <a811219@gmail.com>
2021-04-06 14:01:56 +0900

    복잡한 UI 작성

--- lib/4_5/page210.dart
+++ lib/4_5/page210.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -75,14 +76,202 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Page1 extends StatelessWidget {
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  final dummyItems = [
+    'https://blog.kakaocdn.net/dn/chMnQ0/btqDurL9Kwp/oN687RdyAUkt0IQf6U966K/img.png',
+    'https://t1.daumcdn.net/movie/f2a2f4499800518cf7b3eb999bd83c4e1f2da89b',
+    'https://upload.wikimedia.org/wikipedia/commons/7/7d/IU_MelOn_Music_Awards_2017_06.jpg',
+    'https://i.pinimg.com/originals/52/c7/ab/52c7ab7f3825f0b804555681b7c6098b.jpg',
+    'http://www.newsfreezone.co.kr/news/photo/202011/277190_272039_1050.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '0 페이지',
-        style: TextStyle(fontSize: 100),
-      ),
+    return ListView(
+      children: [
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
+    );
+  }
+
+  Widget _buildTop() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: () {
+                print('클릭!!!!!!!');
+                _showDialog();
+              },
+              child: Column(
+                children: [
+                  Image.network(
+                    'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Image.network(
+                  'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Image.network(
+                  'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Image.network(
+                  'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                Text('택시'),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Image.network(
+                  'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Image.network(
+                  'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                Text('택시'),
+              ],
+            ),
+            Column(
+              children: [
+                Image.network(
+                  'https://png.pngtree.com/png-clipart/20190520/original/pngtree-taxi-vector-icon-png-image_3722634.jpg',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+                Text('택시'),
+              ],
+            ),
+            SizedBox(
+              width: 80,
+              height: 80,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMiddle() {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 150,
+        autoPlay: true,
+        aspectRatio: 2.0,
+        enlargeCenterPage: true,
+        autoPlayInterval: Duration(milliseconds: 2000),
+      ), // 높이 400
+      items: dummyItems.map((url) {
+        // 다섯 페이지
+        return Builder(
+          builder: (BuildContext context) {
+            // context를 사용하고자 할 때
+            return Container(
+              width: MediaQuery.of(context).size.width, // 기기의 가로 길이
+              margin: EdgeInsets.symmetric(horizontal: 5.0), // 좌우 여백 5
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildBottom() {
+    final items = List.generate(10, (i) {
+      return ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] 이것은 공지사항입니다'),
+      );
+    });
+
+    return ListView(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Alert Dialog title"),
+          content: new Text("Alert Dialog body"),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -110,4 +299,3 @@ class Page3 extends StatelessWidget {
     );
   }
 }
-
