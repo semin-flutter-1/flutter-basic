7c6e47bb7be540ad63c3e1d56be73348d5b66762
Junsuk Oh <a811219@gmail.com>
2021-05-04 12:17:52 +0900

    데이팅 앱 UI 연습 2 - 상단 이미지 부분

--- lib/5_04/profile_top_section.dart
+++ lib/5_04/profile_top_section.dart
@@ -3,23 +3,66 @@ import 'package:flutter/material.dart';
 
 import 'model/profile.dart';
 
-class ProfileTopSection extends StatelessWidget {
+class ProfileTopSection extends StatefulWidget {
   final Profile profile;
 
   ProfileTopSection(this.profile);
 
   @override
+  _ProfileTopSectionState createState() => _ProfileTopSectionState();
+}
+
+class _ProfileTopSectionState extends State<ProfileTopSection> {
+  int _index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      items: profile.profileImages
-          .map((e) => Image.network(
-                e,
-                width: MediaQuery.of(context).size.width,
-                height: 370,
-                fit: BoxFit.cover,
-              ))
-          .toList(),
-      options: CarouselOptions(height: 370.0),
+    return Stack(
+      children: [
+        CarouselSlider(
+          items: widget.profile.profileImages
+              .map((e) => Image.network(
+                    e,
+                    width: MediaQuery.of(context).size.width,
+                    height: 370,
+                    fit: BoxFit.cover,
+                  ))
+              .toList(),
+          options: CarouselOptions(
+            height: 370.0,
+            viewportFraction: 1,
+            enableInfiniteScroll: false,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _index = index;
+              });
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 30,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widget.profile.profileImages.asMap().entries.map((e) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: e.key == _index
+                        ? Colors.red
+                        : Colors.white,
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
     );
   }
 }
